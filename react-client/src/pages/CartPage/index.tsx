2a6f41005f1f6f113c70d6ed49abdbd63374f30c
baotram <hbaotram01@gmail.com>
2024-04-01 08:42:41 +0700

    update

--- react-client/src/pages/CartPage/index.tsx
+++ react-client/src/pages/CartPage/index.tsx
@@ -1,66 +1,74 @@
-import { useParams, useNavigate } from "react-router-dom"
-import axios from "axios";
-import globalConfigs from '../../constants/config'
-import { useQuery } from "@tanstack/react-query";
-import numeral from "numeral";
+import { useNavigate } from "react-router-dom";
 import { useCartStore } from "../../hooks/useCartStore";
-const ProductPage = () => {
-  const {addItem} = useCartStore();
-
-  const params = useParams();
-  const navigate = useNavigate();
-  const slug = params.slug || '';
-  console.log(slug);
-
-  const getProduct = async ()=>{
-    const response = await axios.get(globalConfigs.urlAPI + `/v1/products/slug/${slug}`)
-    return response.data
-  }
-  const {data: product, isSuccess, isError, isLoading, error} = useQuery({ 
-    //Bổ sung các searchParams vào để tạo key không trùng lặp
-    queryKey: ['product-details',slug], 
-    queryFn: getProduct 
-  });
-
-  if(isError){
-    console.log(error.message);
-    navigate('/404')
-  }
-
+import numeral from "numeral";
+const CartPage = () => {
+    const navigate = useNavigate();
+    const {items, increaseQuantity, decreaseQuantity, total} = useCartStore();
   return (
     <div>
-      {
-        isLoading ? (<span>Loading....</span>) : (
-          <div className="product_details flex">
-              <div className="gallery">
-                  <img src={product.data.thumbnail} alt={product.data.productName} />
-              </div>
-              <div className="product_info">
-                  <h1 className="text-2xl">{product.data.productName}</h1>
-                  <div className="price">
-                  <strong>{numeral(product.data.price).format('0,0$')}</strong>
-                  </div>
-                  <div className="">
-                    {product.data.description}
-                  </div>
-                  <div className="actions my-5">
-                      <button onClick={()=>{
-                        addItem({
-                          product: product.data._id,
-                          name: product.data.productName,
-                          price: product.data.price,
-                          quantity: 1,
-                          thumb: product.data.thumbnail
-                        })
-                      }}  className="bg-indigo-700 hover:bg-indigo-900 text-white py-3 px-5 rounded">Add To Cart</button>
-                  </div>
-              </div>
-          </div>
-        )
-      }
-      <h1></h1>
+        <div className="bg-gray-100 h-screen py-8">
+    <div className="container mx-auto px-4">
+        <h1 className="text-2xl font-semibold mb-4">Shopping Cart</h1>
+        <div className="flex flex-col md:flex-row gap-4">
+            <div className="md:w-full">
+                <div className="bg-white rounded-lg shadow-md p-6 mb-4">
+                    <table className="w-full">
+                        <thead>
+                            <tr>
+                                <th className="text-left font-semibold">Product</th>
+                                <th className="text-left font-semibold">Price</th>
+                                <th className="text-left font-semibold">Quantity</th>
+                                <th className="text-left font-semibold">Total</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                items.length > 0 && items.map((product)=>{
+                                    return <tr>
+                                    <td className="py-4">
+                                        <div className="flex items-center">
+                                            <img className="h-16 w-16 mr-4" src={product.thumb} alt="Product image" />
+                                            <span className="font-semibold">{product.name}</span>
+                                        </div>
+                                    </td>
+                                    <td className="py-4">{numeral(product.price).format('0,0$')}</td>
+                                    <td className="py-4">
+                                        <div className="flex items-center">
+                                            <button onClick={()=>{
+                                                decreaseQuantity(product.id)
+                                            }} className="border rounded-md py-2 px-4 mr-2">-</button>
+                                            <span className="text-center w-8">{product.quantity}</span>
+                                            <button onClick={()=>{
+                                                increaseQuantity(product.id)
+                                            }} className="border rounded-md py-2 px-4 ml-2">+</button>
+                                        </div>
+                                    </td>
+                                    <td className="py-4">{numeral(product.price * product.quantity).format('0,0$')}</td>
+                                </tr>
+                                })
+                            }
+                            <tr>
+                                <td>Total</td>
+                                <td colSpan={3} className="text-right">{numeral(total).format('0,0$')}</td>
+                            </tr>
+                            <tr>
+                                <td>
+                                <button onClick={()=>{
+                        navigate('/checkout')
+                    }} className="bg-blue-500 text-white py-2 px-4 rounded-lg mt-4 w-full">Checkout</button>
+
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</div>
     </div>
   )
 }
 
-export default ProductPage
+export default CartPage
