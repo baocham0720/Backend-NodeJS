c7d8fca498b4f9da33953a5a690812abe34f84de
baotram <hbaotram01@gmail.com>
2024-04-08 10:33:34 +0700

    up

--- express-typeorm/app.ts
+++ express-typeorm/app.ts
@@ -1,15 +1,18 @@
-import express from 'express'
+import express from "express"
 import { Request, Response, NextFunction } from "express"
 import bodyParser from "body-parser"
 import routerProduct from "./routes/products.route"
 import routerCategories from "./routes/categories.route"
 import routerBrand from "./routes/brand.route"
 import createError  from 'http-errors';
+import cors from 'cors'
 // create and setup express app
 const app = express()
 app.use(bodyParser.json())
-//app.use(express.json());
+// app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(cors({ origin: '*' })); //Cho phép gọi bất kỳ đâu
 
 app.use('/products', routerProduct)
 app.use('/categories', routerCategories)
